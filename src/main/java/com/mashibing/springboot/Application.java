1d5ea3d4c211874ca0246f85e6644f5e4940937d
Forever <forever@ForeverdeMacBook-Pro.local>
2024-08-11 20:25:00 +0800

    定义servletbean

--- src/main/java/com/mashibing/springboot/Application.java
+++ src/main/java/com/mashibing/springboot/Application.java
@@ -1,13 +1,26 @@
 package com.mashibing.springboot;
 
+import com.mashibing.springboot.servlet.MyServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+
+    //将servlet注册为Spring中的一个Bean
+    @Bean
+    public ServletRegistrationBean<MyServlet> getServletRegistrationBean(){
+        ServletRegistrationBean<MyServlet> bean = new ServletRegistrationBean<MyServlet>(new MyServlet(),"/s2");
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
 }
---
+++ src/main/java/com/mashibing/springboot/servlet/MyServlet.java
@@ -0,0 +1,20 @@
+package com.mashibing.springboot.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "myservlet",urlPatterns = "/srv")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("111");
+        super.doGet(req, resp);
+    }
+
+
+}
